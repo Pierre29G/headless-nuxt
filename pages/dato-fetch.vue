4a7cbb04ac798868ef55be40b0af40807e1b5bf2
Pierre29G <pierre29g@gmail.com>
2023-02-24 15:13:34 +0100

    better dato-fecth

--- pages/dato-fetch.vue
+++ pages/dato-fetch.vue
@@ -1,12 +1,5 @@
 <template>
     <h1>Dato usage</h1>
-    <div v-if="headerPending">
-        <p>Loading data</p>
-    </div>
-    <div v-else>
-        <h2>Blog header</h2>
-        <p>Site name: {{ cmsHeader?.header?.siteName }}</p>
-    </div>
 
     <div v-if="postsPending">
         <p>Loading data</p>
@@ -14,28 +7,17 @@
     <div v-else>
         <h2>Blog results</h2>
         <ul>
-            <li v-for="post in blogPosts.allBlogPosts" :key="post.id">
-                <nuxt-link :to="{name: 'blog-slug', params: {slug: post.blogSlug}}">{{ post.blogTitle }}</nuxt-link>
+            <li v-for="post in blogPosts.allRecipes" :key="post.id">
+                <img :src="post.recipeImg.url" alt="chat" />
+                <nuxt-link :to="{name: 'blog-slug', params: {slug: post.slugUrl}}">{{ post.title }}</nuxt-link>
             </li>
         </ul>
     </div>
 
-    <div v-if="footerPending">
-        <p>Loading data</p>
-    </div>
-    <div v-else>
-        <h2>Blog footer</h2>
-        <p v-for="item in cmsFooter.footer.differentUsage" :key="item.id">{{ item.title }}</p>
-    </div>
 </template>
 
 <script setup>
     import blog_post from '@/cms/queries/blog_post';
-    import header from '@/cms/queries/header';
-    import footer from '@/cms/queries/footer';
-
 
     const { data: blogPosts, pending: postsPending } = await useLazyAsyncQuery(blog_post)
-    const { data: cmsHeader, pending: headerPending } = await useLazyAsyncQuery(header)
-    const { data: cmsFooter, pending: footerPending } = await useLazyAsyncQuery(footer)
 </script>
