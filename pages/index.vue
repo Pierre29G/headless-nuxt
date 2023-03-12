29f6217dfb0354e77f35440792b2813a532ed97d
Pierre29G <pierre29g@gmail.com>
2023-03-12 04:10:37 +0100

    Cleaning index links

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,7 @@
 <template>
     <ul>
-        <li><NuxtLink to="/about">About page</NuxtLink></li>
-        <li><NuxtLink to="/store-usage">Storage usage</NuxtLink></li>
-        <li><NuxtLink to="/computed-usage">Computed usage</NuxtLink></li>
-        <li><NuxtLink :to="{name: 'router-page'}">Router usage</NuxtLink></li>
-        <li><NuxtLink :to="{name: 'dynamic-router-page', params: {slug: 'admin'}}">Dynamic router usage</NuxtLink></li>
-        <li><NuxtLink :to="{name: 'blog-slug', params: {slug: 'test'}}">Blog usage</NuxtLink></li>
         <li><NuxtLink :to="{name: 'fetch-usage'}">Fetch usage</NuxtLink></li>
-        <li><NuxtLink :to="{name: 'dato-fetch'}">Dato usage</NuxtLink></li>
+        <li><NuxtLink :to="{name: 'archive'}">Dato usage</NuxtLink></li>
         <li><NuxtLink :to="{name: 'style-usage'}">Style usage</NuxtLink></li>
     </ul>
     <h1>Home page</h1>
@@ -18,7 +12,4 @@
 </template>
 
 <script setup>
-    definePageMeta({
-        layout: 'layout-2'
-    })
 </script>
