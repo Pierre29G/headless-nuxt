c3d015c893869bb6571e99199d746265b485fa6b
Pierre29G <pierre29g@gmail.com>
2023-03-12 02:28:03 +0100

    Base call for blog single

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,4 +1,24 @@
 <template>
     <h1>Blog</h1>
-    <p>Article: {{ $route.params.slug }}</p>
+
+    <div v-if="postsPending">
+        <p>Loading data</p>
+    </div>
+    <div v-else>
+        <p>{{ recipeData.recipe }}</p>
+    </div>
+    
 </template>
+
+<script setup>
+    import blog_single from '@/cms/queries/blog_single';
+
+    const route = useRoute();
+    
+    const variable = {
+        slugUrl : route.params.slug
+    }
+
+
+    const { data: recipeData, pending: postsPending } = await useLazyAsyncQuery(blog_single, variable)
+</script>
